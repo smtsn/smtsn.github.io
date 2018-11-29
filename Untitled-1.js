0fb7990455389ffd701149d67b36acb59232211e
smtsn <45443068+smtsn@users.noreply.github.com>
2018-11-29 12:32:12 +0800

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,15 @@
+// JavaScript Document<script type="text/javascript">
+<script language="JavaScript1.2">
+<!--
+top.window.moveTo(0,0);
+if (document.all) {
+top.window.resizeTo(screen.availWidth,screen.availHeight);
+}
+else if (document.layers||document.getElementById) {
+if (top.window.outerHeight<screen.availHeight||top.window.outerWidth<screen.availWidth){
+top.window.outerHeight = screen.availHeight;
+top.window.outerWidth = screen.availWidth;
+}
+}
+//-->
+</script>
